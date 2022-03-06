516e455adc4d53ddec3a4341f189709beefa7c64
ukshishir <usukshishir@gmail.com>
2022-03-06 22:35:56 +0600

    It's my fast commit

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ window.onload = () => {
 function main(){
     const root = document.querySelector('.container')
     const input= document.querySelector('.input-copy');
+    const input2= document.querySelector('.input-rgb');
     const copyBtn= document.querySelector('.copy-btn');
     const changeBtn= document.querySelector('#change-btn');
  
@@ -19,9 +20,12 @@ function main(){
  // changeTheBackgroundColor and  CopyInput
 
     changeBtn.addEventListener('click', function(){
-        bg = genareatHexColor();
-        root.style.background= bg;
-        input.value= bg.substring(1);
+       const color = generateDecimalCode();
+       const hex = genareatHexColor(color);
+       const rgb = generatRgbColor(color);
+        root.style.background= hex;
+        input.value= hex.substring(1).toUpperCase();
+        input2.value = rgb
     })
 
 // Copy Input Value
@@ -38,8 +42,12 @@ copyBtn.addEventListener('click', function(){
 // isValidHex key  in input 
 input.addEventListener('keyup', function(e){
     const color = e.target.value;
-    if(color && isValied){
-        root.style.background = `#${color}`;
+
+    if(color){
+        input.value = color.toUpperCase()
+        if(isValiedHex(color)){
+            root.style.backgroundColor = `#${color}`
+        }
     }
 })
 
@@ -47,14 +55,44 @@ input.addEventListener('keyup', function(e){
 
 console.log(main())
 
-function genareatHexColor(){
+
+function generateDecimalCode(){
     const red = Math.floor(Math.random()* 255);
     const green = Math.floor(Math.random()* 255);
     const blue = Math.floor(Math.random()* 255);
 
-    return  `#${red.toString(16) }${green.toString(16) }${blue.toString(16)}`
+    return{
+        red,
+        green, 
+        blue,
+    }
+}
+
+
+// generat Hexadecimal color code function one
+
+function genareatHexColor({red, green, blue}){
+    
+    // const {red, green, blue} = generateDecimalCode()
+
+    const getTwoCode = (value) => {
+        const hex = value.toString(16)
+        return hex.length === 1 ? `0${hex}` : hex;
+    }
+
+    return  `#${getTwoCode(red)}${getTwoCode(green)}${getTwoCode(blue)}`
 } 
 
+// Generat RGB color code funtion 
+
+function generatRgbColor({red, green, blue}){
+    
+    const {red, green, blue} = generateDecimalCode()
+
+    return `rgb(${red},${green},${blue})`
+}
+
+// generate tost message color function 
 
 function genarateTostMsg(msg){
     div = document.createElement('div');
@@ -77,7 +115,7 @@ function genarateTostMsg(msg){
  * @param {string} color : ;
  */
 
-function isValied(color){
+function isValiedHex(color){
     if(color.length !== 6) return false;
     // if (color[0] !== '#') return false;
 
@@ -88,4 +126,7 @@ function isValied(color){
 
 
 
+// Step-1: Creat one hendeler 
+
+// Step-2: 
 
